34bf781502bdd063bbce0cb9fd2388127b465277
Mike Wallbank <m.wallbank@sheffield.ac.uk>
2016-08-03 11:54:44 -0500

    Updated job submission scripts. Updated energy conversion scripts

--- 35tanalysis/obtain_analyse_files.sh
+++ 35tanalysis/obtain_analyse_files.sh
@@ -2,84 +2,43 @@
 
 # Script to get files out of samweb matching runs we're interested in and run analysis over them
 
-outputDirTag=both_nofilter
-jobDir=jobs_nf
-filter=0
+if [ -z $1 ]; then
+    echo
+    echo "Please provide an input file with the run numbers of interest!"
+    echo
+fi
 
-while read run; do
+export OUTPUTDIRTAG=EW
+export RUNFILE=ew_filter_runs.txt
+export FILTER=1
+
+# make sure output dir is empty
+export OUTDIR=/pnfs/dune/persistent/users/wallbank/Sliced_And_Filtered/${OUTPUTDIRTAG}
+touch $OUTDIR
+rm -rf $OUTDIR
+mkdir $OUTDIR
+
+RUNLIST=()
+
+while read line; do
 
     # ignore the line if it's a comment or empty line
-    if [[ $run == \#* ]] || [[ -z $run ]]; then
+    if [[ $line == \#* ]] || [[ -z $line ]]; then
 	continue
     fi
+    # if the line is "STOP", stop processing
+    if [[ $line == STOP ]]; then
+	break
+    fi
 
-    echo Analysing run $run
-
-    # get the file from sam web
-    filename=`samweb list-files "run_number=$run and data_tier raw"`
-    path=`samweb -e lbne get-file-access-url "$filename"`
-    file=/pnfs/lbne${path:29}
-
-    # get the directories
-    outDir=/dune/data/users/wallbank/Sliced_And_Filtered/${outputDirTag}/${run}
-    condorDir='${_CONDOR_SCRATCH_DIR}'/${outputDirTag}/${run}
-
-    # make output dir
-    touch ${outDir}
-    rm -rf ${outDir}
-    mkdir ${outDir}
-
-    # write a script to run the job
-    cat > "${jobDir}/job_r${run}.sh" <<EOF
-#!/bin/bash
-
-echo Start of analysing run $run
-
-alias copyfile="ifdh cp --force=expgridftp"
-
-# setup the software
-source /grid/fermiapp/products/dune/setup_dune.sh
-source ${MRB_INSTALL}/setup
-#source ${MRB_DIR}/bin/mrbSetEnv
-source ${MRB_DIR}/bin/setup_local_products
-
-# copy the necessary files
-mkdir -p $condorDir
-cd $condorDir
-copyfile /dune/app/users/wallbank/larsoft-base/workspace/35tanalysis/RunSplitterDefault.fcl .
-copyfile /dune/app/users/wallbank/larsoft-base/workspace/35tanalysis/SliceAndFilter.fcl .
-copyfile /dune/app/users/wallbank/larsoft-base/workspace/35tanalysis/reco_dune35tdata.fcl .
-copyfile /dune/app/users/wallbank/larsoft-base/workspace/35tanalysis/apa_crossing.fcl .
-copyfile $file .
-
-echo
-echo Contents of directory
-ls
-echo
-
-# run the jobs
-if [ ${filter} -eq 1 ]
-then
-lar -c SliceAndFilter.fcl -s $filename -o sliced_and_filtered.root
-else
-lar -c RunSplitterDefault.fcl -s $filename -o sliced_and_filtered.root
-fi
-lar -c reco_dune35tdata.fcl -s sliced_and_filtered.root -o reco.root
-lar -c apa_crossing.fcl -s reco.root
-
-# copy output files back
-rm -f $filename
-copyfile -r $condorDir $outDir
-
-# remove the temp directory
-rm -rf $condorDir
-
-echo End of script
-
-EOF
-
-    # submit job
-    chmod +x ${jobDir}/job_r${run}.sh
-    jobsub_submit --group=dune --use_gftp --resource-provides=usage_model=OPPORTUNISTIC file:///dune/app/users/wallbank/larsoft-base/workspace/35tanalysis/${jobDir}/job_r${run}.sh
+    RUNLIST=("${RUNLIST[@]}" $line)
 
 done < "$1"
+
+printf "%s\n" "${RUNLIST[@]}" > $RUNFILE
+
+# submit job
+num_jobs=${#RUNLIST[@]}
+echo Submitting $num_jobs jobs
+
+jobsub_submit -N $num_jobs -e MRB_INSTALL -e MRB_DIR -e RUNFILE -e FILTER -e OUTPUTDIRTAG -e OUTDIR --group=dune --use_gftp --resource-provides=usage_model=OPPORTUNISTIC file:///dune/app/users/wallbank/larsoft-base/workspace/35tanalysis/do_obtain_analyse_files.sh

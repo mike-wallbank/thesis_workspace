e8fdcbee9e9a7978c35541f5251ccf8cc3f5fa59
Mike Wallbank <m.wallbank@sheffield.ac.uk>
2016-08-08 07:23:36 -0500

    updated to workspace, with the shower energy conversion code now all being correct and up-to-date

--- 35tanalysis/obtain_analyse_files.sh
+++ 35tanalysis/obtain_analyse_files.sh
@@ -8,13 +8,15 @@ if [ -z $1 ]; then
     echo
 fi
 
-export OUTPUTDIRTAG=EW
-export RUNFILE=ew_filter_runs.txt
-export FILTER=1
+export OUTPUTDIRTAG=both_nofilter
+export RUNFILE=both_nofilter_runs.txt
+export FILTER=0
 
 # make sure output dir is empty
 export OUTDIR=/pnfs/dune/persistent/users/wallbank/Sliced_And_Filtered/${OUTPUTDIRTAG}
+echo Ensuring empty output directory exists at $OUTDIR
 touch $OUTDIR
+chmod +xrw $OUTDIR
 rm -rf $OUTDIR
 mkdir $OUTDIR
 
--- energyConversion/35t/getEnergyConversion.C
+++ energyConversion/35t/getEnergyConversion.C
@@ -40,8 +40,8 @@
 #include "TMath.h"
 
 const int kMaxHits = 10000;
-const double kEnergy[10] = { 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0 };
-const double kdE = 0.5;
+const double kEnergy[10] = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 };
+const double kdE = 0.1;
 
 class EMEnergyConversion {
 public:

3407247bd434b4db5f998c1d99fef8d9fdff474c
Mike Wallbank <m.wallbank@sheffield.ac.uk>
2016-12-13 08:42:29 -0600

    fix to 35ton job script

--- 35tanalysis/do_obtain_analyse_files.sh
+++ 35tanalysis/do_obtain_analyse_files.sh
@@ -11,6 +11,7 @@ source ${MRB_DIR}/bin/setup_local_products
 # make a temp dir in condor scratch
 CONDORDIR=${_CONDOR_SCRATCH_DIR}/${outputDirTag}
 mkdir -p ${CONDORDIR}
+echo Made directory at ${CONDORDIR}
 
 # get the run number for this job
 copyfile /dune/app/users/wallbank/larsoft-base/workspace/35tanalysis/$RUNFILE ${CONDORDIR}
@@ -27,6 +28,8 @@ rm -rf ${OUTRUNDIR}
 mkdir ${OUTRUNDIR}
 mkdir -p ${CONDORRUNDIR}
 
+echo Made directory at ${CONDORRUNDIR}
+
 # get the file from sam web
 filename=`samweb list-files "run_number=$run and data_tier raw"`
 path=`samweb -e lbne get-file-access-url "$filename"`
@@ -34,12 +37,16 @@ file=/pnfs/lbne${path:29}
 
 # copy the necessary files
 cd $CONDORRUNDIR
-copyfile /dune/app/users/wallbank/larsoft-base/workspace/job/RunSplitter.fcl .
-copyfile /dune/app/users/wallbank/larsoft-base/workspace/job/SliceAndFilter.fcl .
-copyfile /dune/app/users/wallbank/larsoft-base/workspace/job/reco_dune35tdata.fcl .
-#copyfile /dune/app/users/wallbank/larsoft-base/workspace/job/apa_crossing.fcl .
-#copyfile /dune/app/users/wallbank/larsoft-base/workspace/35tanalysis/bad_timings.fcl .
-copyfile $file .
+echo
+echo About to copy files into directory
+pwd
+echo
+copyfile /dune/app/users/wallbank/larsoft-base/workspace/job/RunSplitter.fcl RunSplitter.fcl
+copyfile /dune/app/users/wallbank/larsoft-base/workspace/job/SliceAndFilter.fcl SliceAndFilter.fcl
+copyfile /dune/app/users/wallbank/larsoft-base/workspace/job/reco_dune35tdata.fcl reco_dune35tdata.fcl
+#copyfile /dune/app/users/wallbank/larsoft-base/workspace/job/apa_crossing.fcl apa_crossing.fcl
+#copyfile /dune/app/users/wallbank/larsoft-base/workspace/35tanalysis/bad_timings.fcl bad_timings.fcl
+copyfile $file $filename
 
 echo "About to run jobs, contents of directory is"
 ls
@@ -49,7 +56,7 @@ if [ ${FILTER} -eq 1 ]
 then
 lar -c SliceAndFilter.fcl -s $filename -o sliced_and_filtered.root -n 5
 else
-lar -c RunSplitterDefault.fcl -s $filename -o sliced_and_filtered.root -n 5
+lar -c RunSplitter.fcl -s $filename -o sliced_and_filtered.root -n 5
 fi
 lar -c reco_dune35tdata.fcl -s sliced_and_filtered.root -o reco.root
 #lar -c apa_crossing.fcl -s reco.root
